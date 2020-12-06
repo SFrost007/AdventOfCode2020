6de9bfd1e4438bae80eb5ff591687cd99b9a6957
Simon Frost <sfrost007@gmail.com>
2020-12-06 08:53:51 +0000

    Day 6: Add tests and class structure

---
+++ AdventOfCode2020/Day6.swift
@@ -0,0 +1,36 @@
+//
+//  Day6.swift
+//  AdventOfCode2020
+//
+//  Created by Simon Frost on 06/12/2020.
+//
+
+import Foundation
+
+class Day6 {
+    
+    // MARK: - Initialisation
+    
+    let inputData: [String]
+    
+    init(inputURL: URL) {
+        inputData = try! String(contentsOf: inputURL).components(separatedBy: "\n\n")
+    }
+    
+    // MARK: - Problem cases
+    
+    func part1() -> Int {
+        return inputData.map { Self.uniqueAnswers(in: $0) }.reduce(0, +)
+    }
+    
+    func part2() -> Int {
+        fatalError("Not implemented")
+    }
+    
+    // MARK: - Worker functions
+    
+    static func uniqueAnswers(in input: String) -> Int {
+        return 0
+    }
+    
+}
---
+++ AdventOfCode2020Tests/Day6Tests.swift
@@ -0,0 +1,38 @@
+//
+//  Day6Tests.swift
+//  AdventOfCode2020Tests
+//
+//  Created by Simon Frost on 06/12/2020.
+//
+
+import XCTest
+@testable import AdventOfCode2020
+
+class Day6Tests: AoCTestCase {
+    
+    let example = Day6(inputURL: urlForExampleData(day: 6))
+    let myInput = Day6(inputURL: urlForMyInputData(day: 6))
+    
+    func testPart1() {
+        XCTAssertEqual(Day6.uniqueAnswers(in: example.inputData[0]), 3)
+        XCTAssertEqual(Day6.uniqueAnswers(in: example.inputData[1]), 3)
+        XCTAssertEqual(Day6.uniqueAnswers(in: example.inputData[2]), 3)
+        XCTAssertEqual(Day6.uniqueAnswers(in: example.inputData[3]), 1)
+        XCTAssertEqual(Day6.uniqueAnswers(in: example.inputData[4]), 1)
+        XCTAssertEqual(example.part1(), 11)
+        
+        print("Answer for Day 5 Part 1: \(myInput.part1())")
+        measure {
+            let _ = myInput.part1()
+        }
+    }
+    
+    func testPart2() {
+        XCTFail()
+        print("Answer for Day 5 Part 2: \(myInput.part2())")
+        measure {
+            let _ = myInput.part2()
+        }
+    }
+
+}
