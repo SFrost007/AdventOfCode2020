1ee504ef28b4db323ed4826c2ee24287759c575b
Simon Frost <sfrost007@gmail.com>
2020-12-07 11:09:24 +0000

    Day 7: Working input parsing

--- AdventOfCode2020/Day7.swift
+++ AdventOfCode2020/Day7.swift
@@ -9,18 +9,30 @@ import Foundation
 
 class Day7 {
     
+    struct ContainedBag: Hashable {
+        let color: String
+        let count: Int
+    }
+    
+    struct Instruction: Equatable {
+        let containerBagColor: String
+        let innerBags: [ContainedBag]
+    }
+    
     // MARK: - Initialisation
     
-    let inputData: [String]
+    let inputData: [Instruction]
     
     init(inputURL: URL) {
         inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .map { Self.parseInstruction($0) }
     }
     
     // MARK: - Problem cases
     
     func part1() -> Int {
-        fatalError("Not yet implemented")
+        return Self.findAllowedOuterBags(for: ["shiny gold"], in: inputData).count
     }
     
     func part2() -> Int {
@@ -29,8 +41,30 @@ class Day7 {
     
     // MARK: - Worker functions
     
-    static func findSomething(in input: String) -> Int {
-        return -1
+    private static func parseInstruction(_ input: String) -> Instruction {
+        // e.g. input:
+        //  bright white bags contain 1 shiny gold bag.
+        //  shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        //  faded blue bags contain no other bags.
+        let outerSplit = input.components(separatedBy: " bags contain ")
+        guard !outerSplit[1].contains("no other") else {
+            return Instruction(containerBagColor: outerSplit[0], innerBags: [])
+        }
+        let innerParts = outerSplit[1].components(separatedBy: ", ")
+            .map { $0.replacingOccurrences(of: ".", with: "") }
+            .map { $0.replacingOccurrences(of: " bags", with: "") }
+            .map { $0.replacingOccurrences(of: " bag", with: "") } // TODO: Would be better as a regex
+        let innerBags = innerParts.map { (rawInput: String) -> ContainedBag in
+            let parts = rawInput.components(separatedBy: .whitespaces)
+            let count = Int(parts[0])!
+            let color = parts[1...].joined(separator: " ")
+            return ContainedBag(color: color, count: count)
+        }
+        return Instruction(containerBagColor: outerSplit[0], innerBags: innerBags)
+    }
+    
+    static func findAllowedOuterBags(for colors: [String], in instructions: [Instruction]) -> [String] {
+        return []
     }
     
 }
--- AdventOfCode2020Tests/Day7Tests.swift
+++ AdventOfCode2020Tests/Day7Tests.swift
@@ -13,9 +13,18 @@ class Day7Tests: AoCTestCase {
     let example = Day7(inputURL: urlForExampleData(day: 7))
     let myInput = Day7(inputURL: urlForMyInputData(day: 7))
     
+    func testParsing() {
+        XCTAssertEqual(example.inputData.count, 9)
+        let inner1 = Day7.ContainedBag(color: "bright white", count: 1)
+        let inner2 = Day7.ContainedBag(color: "muted yellow", count: 2)
+        let outer = Day7.Instruction(containerBagColor: "light red", innerBags: [inner1, inner2])
+        XCTAssertEqual(example.inputData[0], outer)
+    }
+    
     func testPart1() {
-        XCTAssertEqual(Day7.findSomething(in: example.inputData[0]), 123)
-        XCTAssertEqual(example.part1(), 123)
+        XCTAssertEqual(Day7.findAllowedOuterBags(for: ["shiny gold"], in: example.inputData),
+                       ["bright white", "muted yellow", "dark orange", "light red"])
+        XCTAssertEqual(example.part1(), 4)
         
         print("Answer for Day 7 Part 1: \(myInput.part1())")
         measure {
@@ -24,7 +33,6 @@ class Day7Tests: AoCTestCase {
     }
     
     func testPart2() {
-        XCTAssertEqual(Day7.findSomething(in: example.inputData[0]), 123)
         XCTAssertEqual(example.part2(), 123)
         
         print("Answer for Day 7 Part 2: \(myInput.part2())")
