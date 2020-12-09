7c13505031407e5481092f5d995bcc366826c38a
Simon Frost <sfrost007@gmail.com>
2020-12-09 09:28:54 +0000

    Day 9: Add input parsing and TDD for part 1

--- AdventOfCode2020/Day9.swift
+++ AdventOfCode2020/Day9.swift
@@ -11,16 +11,16 @@ class Day9 {
     
     // MARK: - Initialisation
     
-    let inputData: [String]
+    let inputData: [Int]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines).compactMap { Int($0) }
     }
     
     // MARK: - Problem cases
     
-    func part1() -> Int {
-        fatalError("Not yet implemented")
+    func part1(preamble: Int) -> Int {
+        return Self.findFirstInvalid(in: inputData, preamble: preamble)
     }
     
     func part2() -> Int {
@@ -29,8 +29,8 @@ class Day9 {
     
     // MARK: - Worker functions
     
-    static func findSomething(in input: String) -> Int {
-        return -1
+    static func findFirstInvalid(in input: [Int], preamble: Int) -> Int {
+        fatalError("Not yet implemented")
     }
     
 }
--- AdventOfCode2020Tests/Day9Tests.swift
+++ AdventOfCode2020Tests/Day9Tests.swift
@@ -14,17 +14,15 @@ class Day9Tests: AoCTestCase {
     let myInput = Day9(inputURL: urlForMyInputData(day: 9))
     
     func testPart1() {
-        XCTAssertEqual(Day9.findSomething(in: example.inputData[0]), 123)
-        XCTAssertEqual(example.part1(), 123)
+        XCTAssertEqual(example.part1(preamble: 5), 127)
         
-        print("Answer for Day 9 Part 1: \(myInput.part1())")
+        print("Answer for Day 9 Part 1: \(myInput.part1(preamble: 25))")
         measure {
-            let _ = myInput.part1()
+            let _ = myInput.part1(preamble: 25)
         }
     }
     
     func testPart2() {
-        XCTAssertEqual(Day9.findSomething(in: example.inputData[0]), 123)
         XCTAssertEqual(example.part2(), 123)
         
         print("Answer for Day 9 Part 2: \(myInput.part2())")
