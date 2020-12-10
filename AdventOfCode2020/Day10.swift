02eb512492d6fe1be11a13248d994bb22fbf1dec
Simon Frost <sfrost007@gmail.com>
2020-12-10 08:50:53 +0000

    Day 10: Add TDD and signatures

--- AdventOfCode2020/Day10.swift
+++ AdventOfCode2020/Day10.swift
@@ -11,16 +11,17 @@ class Day10 {
     
     // MARK: - Initialisation
     
-    let inputData: [String]
+    let inputData: [Int]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines).compactMap { Int($0) }
     }
     
     // MARK: - Problem cases
     
     func part1() -> Int {
-        fatalError("Not yet implemented")
+        let joltageJumps = Self.findJoltageJumps(in: inputData)
+        return joltageJumps.0 * joltageJumps.2
     }
     
     func part2() -> Int {
@@ -29,8 +30,8 @@ class Day10 {
     
     // MARK: - Worker functions
     
-    static func findSomething(in input: String) -> Int {
-        return -1
+    static func findJoltageJumps(in input: [Int]) -> (Int, Int, Int) {
+        fatalError("Not yet implemented")
     }
     
 }
--- AdventOfCode2020Tests/Day10Tests.swift
+++ AdventOfCode2020Tests/Day10Tests.swift
@@ -14,8 +14,10 @@ class Day10Tests: AoCTestCase {
     let myInput = Day10(inputURL: urlForMyInputData(day: 10))
     
     func testPart1() {
-        XCTAssertEqual(Day10.findSomething(in: example.inputData[0]), 123)
-        XCTAssertEqual(example.part1(), 123)
+        let exampleJumps = Day10.findJoltageJumps(in: example.inputData)
+        XCTAssertEqual(exampleJumps.0, 22)
+        XCTAssertEqual(exampleJumps.2, 10)
+        XCTAssertEqual(example.part1(), 220)
         
         print("Answer for Day 10 Part 1: \(myInput.part1())")
         measure {
@@ -24,7 +26,6 @@ class Day10Tests: AoCTestCase {
     }
     
     func testPart2() {
-        XCTAssertEqual(Day10.findSomething(in: example.inputData[0]), 123)
         XCTAssertEqual(example.part2(), 123)
         
         print("Answer for Day 10 Part 2: \(myInput.part2())")
