4fdc9256005a3f4b227effc1e51fd6257121614e
Simon Frost <sfrost007@gmail.com>
2020-12-12 09:36:32 +0000

    Day 12: Add boilerplate + input data

---
+++ AdventOfCode2020Tests/Day12Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Day12Tests.swift
+//  AdventOfCode2020Tests
+//
+//  Created by Simon Frost on 12/12/2020.
+//
+
+import XCTest
+@testable import AdventOfCode2020
+
+class Day12Tests: AoCTestCase {
+    
+    let example = Day12(inputURL: urlForExampleData(day: 12))
+    let myInput = Day12(inputURL: urlForMyInputData(day: 12))
+    
+    func testPart1() {
+        XCTAssertEqual(Day12.findSomething(in: example.inputData[0]), 123)
+        XCTAssertEqual(example.part1(), 123)
+        
+        print("Answer for Day 12 Part 1: \(myInput.part1())")
+        measure {
+            let _ = myInput.part1()
+        }
+    }
+    
+    func testPart2() {
+        XCTAssertEqual(Day12.findSomething(in: example.inputData[0]), 123)
+        XCTAssertEqual(example.part2(), 123)
+        
+        print("Answer for Day 12 Part 2: \(myInput.part2())")
+        measure {
+            let _ = myInput.part2()
+        }
+    }
+
+}
