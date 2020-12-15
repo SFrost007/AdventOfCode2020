5f0eb002ab06f22c355dcfb9044c9fb0b368db78
Simon Frost <sfrost007@gmail.com>
2020-12-15 14:24:14 +0000

    Day 15: Solution for part 1

--- AdventOfCode2020/Day15.swift
+++ AdventOfCode2020/Day15.swift
@@ -12,7 +12,22 @@ class Day15 {
     // MARK: - Problem cases
     
     static func part1(input: [Int]) -> Int {
-        fatalError("Not yet implemented")
+        var lastTurnCalled: [Int: Int] = input.enumerated().reduce(into: [:]) { (dict, tuple) in
+            let (index, value) = tuple
+            dict[value] = index+1 // Turns are 1-based
+        }
+        var lastNumberCalled = input.last!
+        var nextNumber = 0 // Assumes values never repeat in the input, i.e. each input number is only called once
+        for turn in (input.count+1)...2020 {
+            lastNumberCalled = nextNumber
+            if let lastTimeCalled = lastTurnCalled[lastNumberCalled] {
+                nextNumber = turn - lastTimeCalled
+            } else {
+                nextNumber = 0
+            }
+            lastTurnCalled[lastNumberCalled] = turn
+        }
+        return lastNumberCalled
     }
     
     func part2() -> Int {
