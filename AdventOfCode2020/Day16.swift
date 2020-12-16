5a92f1c50cdbccada73e3868d6c1b0002133e6a7
Simon Frost <sfrost007@gmail.com>
2020-12-16 11:27:26 +0000

    Day 16: Add input parsing and tests

--- AdventOfCode2020/Day16.swift
+++ AdventOfCode2020/Day16.swift
@@ -8,13 +8,40 @@
 import Foundation
 
 class Day16 {
+
+    typealias Ticket = [Int]
+
+    struct TicketField {
+        let fieldName: String
+        let validRanges: [Range<Int>]
+
+        init(string: String) {
+            let parts = string.components(separatedBy: ": ")
+            fieldName = parts[0]
+            validRanges = parts[1].components(separatedBy: " or ").map {
+                let numbers = $0.components(separatedBy: "-")
+                let min = Int(numbers[0])!
+                let max = Int(numbers[1])!
+                return Range(min...max)
+            }
+        }
+    }
     
     // MARK: - Initialisation
-    
-    let inputData: [String]
+
+    let fields: [TicketField]
+    let myTicket: Ticket
+    let nearbyTickets: [Ticket]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        let sections: [[String]] = try! String(contentsOf: inputURL)
+            .components(separatedBy: "\n\n")
+            .map { $0.components(separatedBy: .newlines) }
+        fields = sections[0].compactMap { TicketField(string: $0) }
+        myTicket = sections[1][1].components(separatedBy: ",").map { Int($0)! }
+        nearbyTickets = sections[2][1...]
+            .filter { !$0.isEmpty }
+            .map { $0.components(separatedBy: ",").compactMap { Int($0) } }
     }
     
     // MARK: - Problem cases
--- AdventOfCode2020Tests/Day16Tests.swift
+++ AdventOfCode2020Tests/Day16Tests.swift
@@ -12,9 +12,22 @@ class Day16Tests: AoCTestCase {
     
     let example = Day16(inputURL: urlForExampleData(day: 16))
     let myInput = Day16(inputURL: urlForMyInputData(day: 16))
+
+    func testParsing() {
+        XCTAssertEqual(example.fields.count, 3)
+        XCTAssertEqual(example.fields.first?.fieldName, "class")
+        XCTAssertEqual(example.fields.first?.validRanges.count, 2)
+        XCTAssertEqual(example.fields.first?.validRanges.first?.min(), 1)
+        XCTAssertEqual(example.fields.first?.validRanges.first?.max(), 3)
+
+        XCTAssertEqual(example.myTicket, [7,1,14])
+
+        XCTAssertEqual(example.nearbyTickets.count, 4)
+        XCTAssertEqual(example.nearbyTickets.first, [7,3,47])
+        XCTAssertEqual(example.nearbyTickets.last, [38,6,12])
+    }
     
     func testPart1() {
-        XCTAssertEqual(Day16.findSomething(in: example.inputData[0]), 123)
         XCTAssertEqual(example.part1(), 123)
         
         print("Answer for Day 16 Part 1: \(myInput.part1())")
@@ -24,7 +37,6 @@ class Day16Tests: AoCTestCase {
     }
     
     func testPart2() {
-        XCTAssertEqual(Day16.findSomething(in: example.inputData[0]), 123)
         XCTAssertEqual(example.part2(), 123)
         
         print("Answer for Day 16 Part 2: \(myInput.part2())")
